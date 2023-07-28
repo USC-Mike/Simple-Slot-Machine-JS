c1ddfc2c1e15b279d433bdafe396ab24d6dbaea7
Mike Pino <kurtmpino@gmail.com>
2023-07-28 16:12:09 +0800

    first commit

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet
+// 3. Collect bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. Give the user their winnings
+// 7. Play again
